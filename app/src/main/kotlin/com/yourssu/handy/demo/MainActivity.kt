b03bc393078cdc2248fefc857e842420f396a0e5
cometj03 <harryjung1001@gmail.com>
2024-07-16 18:15:36 +0900

    Initial commit

---
+++ app/src/main/kotlin/com/yourssu/handy/demo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yourssu.handy.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {}
+    }
+}
