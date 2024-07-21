abd5f7a7d50a400c4ed4889bc1431cea1016376b
kangyuri1114 <yuri3334@gmail.com>
2024-07-21 17:42:49 +0900

    add: Primitive Colors 추가

---
+++ compose/src/main/kotlin/com/yourssu/handy/compose/foundation/PrimitiveColors.kt
@@ -0,0 +1,40 @@
+package com.yourssu.handy.compose.foundation
+
+import androidx.compose.ui.graphics.Color
+
+// Primitive Colors
+
+// Color/violet
+internal val ColorViolet50 = Color(0xFFECEFFF)
+internal val ColorViolet100 = Color(0xFFDDE1FF)
+internal val ColorViolet200 = Color(0xFFC2C8FF)
+internal val ColorViolet300 = Color(0xFF9CA3FF)
+internal val ColorViolet400 = Color(0xFF7875FF)
+internal val ColorViolet500 = Color(0xFF6B5CFF)
+internal val ColorViolet600 = Color(0xFF5736F5)
+internal val ColorViolet700 = Color(0xFF4B2AD8)
+internal val ColorViolet800 = Color(0xFF3D25AE)
+internal val ColorViolet900 = Color(0xFF342689)
+internal val ColorViolet950 = Color(0xFF211650)
+
+// Color/gray
+internal val ColorGray50 = Color(0xFFF7F8F8)
+internal val ColorGray100 = Color(0xFFF1F1F4)
+internal val ColorGray200 = Color(0xFFE3E4E8)
+internal val ColorGray300 = Color(0xFFB5B9C4)
+internal val ColorGray400 = Color(0xFF949AA8)
+internal val ColorGray500 = Color(0xFF6E7687)
+internal val ColorGray600 = Color(0xFF596070)
+internal val ColorGray700 = Color(0xFF4B505D)
+internal val ColorGray800 = Color(0xFF3F434D)
+internal val ColorGray900 = Color(0xFF373A43)
+internal val ColorGray950 = Color(0xFF25262C)
+
+// Color/neutral
+internal val ColorNeutralBlack = Color(0xFF25262C)
+internal val ColorNeutralWhite = Color(0xFFFFFFFF)
+internal val ColorNeutralTransparent = Color(0x00FFFFFF)
+
+// Color/Status
+internal val ColorStatusRedMain = Color(0xFFFF5C5C)
+internal val ColorStatusRedSub = Color(0xFFFFEBEB)
