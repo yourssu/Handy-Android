d8fc6d820dd6e5835c3d4def505217559090b819
Jung Haesung <48352078+cometj03@users.noreply.github.com>
2024-07-21 14:35:12 +0900

    Add jetpack compose setting (#1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.foundation.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
