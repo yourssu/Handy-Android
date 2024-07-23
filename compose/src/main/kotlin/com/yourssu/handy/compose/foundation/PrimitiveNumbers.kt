d2c0a3ba86d31adfe169ceeaaa9c9bcf23f5a7bd
kangyuri1114 <yuri3334@gmail.com>
2024-07-23 13:30:33 +0900

    add: number 토큰 추가

---
+++ compose/src/main/kotlin/com/yourssu/handy/compose/foundation/PrimitiveNumbers.kt
@@ -0,0 +1,11 @@
+package com.yourssu.handy.compose.foundation
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+// Numbers
+internal val number8 = 8.dp
+internal val number10 = 10.dp
+internal val number12 = 12.dp
+internal val number14 = 14.dp
+internal val number16 = 16.dp
---
+++ compose/src/main/kotlin/com/yourssu/handy/compose/foundation/Radius.kt
@@ -0,0 +1,11 @@
+package com.yourssu.handy.compose.foundation
+
+import androidx.compose.ui.unit.Dp
+
+enum class Radius(val dp: Dp) {
+    XS(number8),
+    S(number10),
+    M(number12),
+    L(number14),
+    XL(number16)
+}
