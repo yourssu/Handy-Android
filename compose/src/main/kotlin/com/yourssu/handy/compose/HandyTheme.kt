491eee8eefc0287a10a5b0e023aed546fe35efba
kangyuri1114 <yuri3334@gmail.com>
2024-07-23 13:42:54 +0900

    feat: HandyTheme 함수 생성

--- compose/src/main/kotlin/com/yourssu/handy/compose/HandyTheme.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/HandyTheme.kt
@@ -1,2 +1,40 @@
 package com.yourssu.handy.compose
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import com.yourssu.handy.compose.foundation.ColorScheme
+import com.yourssu.handy.compose.foundation.LocalColorScheme
+import com.yourssu.handy.compose.foundation.LocalContentColor
+import com.yourssu.handy.compose.foundation.darkColorScheme
+import com.yourssu.handy.compose.foundation.lightColorScheme
+
+@Composable
+fun HandyTheme(
+    isDarkMode: Boolean = isSystemInDarkTheme(),
+    //typography: HandyTypography = HandyTheme.typography,
+    content: @Composable () -> Unit,
+) {
+    val colors = if (isDarkMode) {
+        darkColorScheme
+    } else {
+        lightColorScheme
+    }
+    CompositionLocalProvider(
+        LocalColorScheme provides colors,
+        // LocalTypography provides typography,
+       LocalContentColor provides colors.textBasicPrimary,
+    ) {
+        // content textStyle
+    }
+}
+
+object HandyTheme {
+    val colors: ColorScheme
+        @Composable
+        get() = LocalColorScheme.current
+
+    // val typography: HandyTypography
+    //    @Composable
+    //    get() = LocalTypography.current
+}
