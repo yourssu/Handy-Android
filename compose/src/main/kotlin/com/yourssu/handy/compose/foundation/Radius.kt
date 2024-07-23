f3d519bb561f7fc3ea6428847f5d9faf50117e74
yuri <83583757+kangyuri1114@users.noreply.github.com>
2024-07-23 17:43:46 +0900

    Primitive Color 추가, Color 토큰 생성 (#3)
    * add: Primitive Colors 추가
    
    * add: ColorScheme 추가
    
    * add: ColorScheme 추가
    
    * fix: 오탈자 수정 및 파일 이름 변경
    
    * add: number 토큰 추가
    
    * fix: 주석 수정
    
    * feat: Color 값 CompositionLocal 생성
    
    * feat: HandyTheme 함수 생성
    
    * del: primitiveNumber 변수 삭제
    
    * chore: LocalContentColor staticCompositionLocalOf -> compositionLocalOf 로 변경
    
    * chore: HandyTheme 내 typo 코드 삭제

---
+++ compose/src/main/kotlin/com/yourssu/handy/compose/foundation/Radius.kt
@@ -0,0 +1,12 @@
+package com.yourssu.handy.compose.foundation
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+enum class Radius(dp: Dp) {
+    XS(8.dp),
+    S(10.dp),
+    M(12.dp),
+    L(14.dp),
+    XL(16.dp)
+}
