2ccf5f4010f6017638fac8414cbead0eacda27ec
kangyuri1114 <yuri3334@gmail.com>
2024-07-23 13:30:54 +0900

    fix: 주석 수정

--- compose/src/main/kotlin/com/yourssu/handy/compose/foundation/PrimitiveColors.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/foundation/PrimitiveColors.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 // Primitive Colors
 
-// Color/violet
+// Violet
 internal val ColorViolet50 = Color(0xFFECEFFF)
 internal val ColorViolet100 = Color(0xFFDDE1FF)
 internal val ColorViolet200 = Color(0xFFC2C8FF)
@@ -17,7 +17,7 @@ internal val ColorViolet800 = Color(0xFF3D25AE)
 internal val ColorViolet900 = Color(0xFF342689)
 internal val ColorViolet950 = Color(0xFF211650)
 
-// Color/gray
+// Gray
 internal val ColorGray50 = Color(0xFFF7F8F8)
 internal val ColorGray100 = Color(0xFFF1F1F4)
 internal val ColorGray200 = Color(0xFFE3E4E8)
@@ -30,11 +30,11 @@ internal val ColorGray800 = Color(0xFF3F434D)
 internal val ColorGray900 = Color(0xFF373A43)
 internal val ColorGray950 = Color(0xFF25262C)
 
-// Color/neutral
+// Neutral
 internal val ColorNeutralBlack = Color(0xFF25262C)
 internal val ColorNeutralWhite = Color(0xFFFFFFFF)
 internal val ColorNeutralTransparent = Color(0x00FFFFFF)
 
-// Color/Status
+// Status
 internal val ColorStatusRedMain = Color(0xFFFF5C5C)
 internal val ColorStatusRedSub = Color(0xFFFFEBEB)
