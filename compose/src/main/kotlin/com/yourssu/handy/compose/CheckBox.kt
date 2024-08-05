ffb79c0f2a4507b855f381721c4eef2a5694ee22
LEE YOU BIN <youbeene3409@naver.com>
2024-08-05 14:36:29 +0900

    chore : change disabled text color

--- compose/src/main/kotlin/com/yourssu/handy/compose/CheckBox.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/CheckBox.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.yourssu.handy.compose.foundation.ColorGray300
 import com.yourssu.handy.compose.foundation.HandyTextStyle
 import com.yourssu.handy.compose.foundation.HandyTypography
 
@@ -64,7 +65,8 @@ fun CheckBox(
                 Spacer(modifier = Modifier.width(8.dp))
                 Text(
                     text = text,
-                    style = typo
+                    style = typo,
+                    color = if(enabled) Color.Unspecified else ColorGray300
                 )
             }
         }
