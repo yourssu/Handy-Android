9bb2c804d10a28485686323a18f716db729eddb8
LEE YOU BIN <youbeene3409@naver.com>
2024-08-11 17:53:57 +0900

    chore : add preview

--- app/src/main/kotlin/com/yourssu/handy/demo/CheckBoxPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/CheckBoxPreview.kt
@@ -3,9 +3,9 @@ package com.yourssu.handy.demo
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.yourssu.handy.compose.HandyTheme
 import com.yourssu.handy.compose.CheckBox
 import com.yourssu.handy.compose.CheckBoxSize
+import com.yourssu.handy.compose.HandyTheme
 
 @Preview(showBackground = true)
 @Composable
@@ -17,6 +17,12 @@ private fun CheckBoxPreview() {
                 onCheckedChange = {},
                 sizeType = CheckBoxSize.Small,
                 text = "selected"
+            )
+            CheckBox(
+                checked = true,
+                onCheckedChange = {},
+                text = "selected",
+                contentColor = HandyTheme.colors.buttonFabPrimaryPressed
             )
             CheckBox(
                 checked = false,
