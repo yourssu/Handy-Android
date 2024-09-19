298f689459296965f4cd3f84fe42213df9d9239e
LEE YOU BIN <youbeene3409@naver.com>
2024-09-20 02:50:35 +0900

    feat : make snackbar file

---
+++ compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
@@ -0,0 +1,11 @@
+package com.yourssu.handy.compose
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SnackBar(
+    modifier : Modifier = Modifier
+){
+
+}
