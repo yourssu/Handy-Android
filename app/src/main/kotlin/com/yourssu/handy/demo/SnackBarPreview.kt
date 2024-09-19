435824b8616aed7ac4a58f79103c6fbb8acdc371
LEE YOU BIN <youbeene3409@naver.com>
2024-09-20 06:00:32 +0900

    feat : make ErrorSnackBar

--- app/src/main/kotlin/com/yourssu/handy/demo/SnackBarPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/SnackBarPreview.kt
@@ -8,9 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.yourssu.handy.compose.ErrorSnackBar
 import com.yourssu.handy.compose.HandyTheme
 import com.yourssu.handy.compose.InfoSnackBar
-import com.yourssu.handy.compose.Text
 
 @Preview(showBackground = true)
 @Composable
@@ -25,6 +25,24 @@ fun InfoSnackBarPreview() {
             Spacer(modifier = Modifier.height(10.dp))
             InfoSnackBar(
                 text = "줄 수가 두 줄 이상이 되는 스낵바 메시지입니다.\n좌측 정렬을 해주세요."
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ErrorSnackBarPreview() {
+    HandyTheme {
+        Column(
+            modifier = Modifier.padding(10.dp)
+        ) {
+            ErrorSnackBar(
+                text = "에러 메세지가 들어갑니다"
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            ErrorSnackBar(
+                text = "두 줄 이상의 에러 메세지가 들어갈 경우\n 아이콘은 모두 위로 정렬해주세요."
             )
         }
     }
--- compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
@@ -1,17 +1,21 @@
 package com.yourssu.handy.compose
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.yourssu.handy.compose.foundation.ColorGray800
+import com.yourssu.handy.compose.foundation.ColorStatusRedSub
+import com.yourssu.handy.compose.foundation.HandyTypography
 
 @Composable
 fun InfoSnackBar(
@@ -28,30 +32,41 @@ fun InfoSnackBar(
     ) {
         Text(
             text = text,
-            color = White,
-            maxLines = 2
+            color = HandyTheme.colors.textBasicWhite,
+            maxLines = 2,
+            style = HandyTypography.B3Sb14
         )
     }
 }
 
-//@Composable
-//fun ErrorSnackBar(
-//    text: String,
-//    modifier: Modifier = Modifier,
-//) {
-//    Row(
-//        modifier = modifier
-//            .padding(horizontal = 16.dp)
-//            .fillMaxWidth()
-//            .clip(RoundedCornerShape(12.dp))
-//            .background(ColorGray800)
-//            .padding(16.dp)
-//    ) {
-//
-//        Text(
-//            text = text,
-//            color = White,
-//            maxLines = 2
-//        )
-//    }
-//}
+@Composable
+fun ErrorSnackBar(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .padding(horizontal = 16.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(12.dp))
+            .background(ColorStatusRedSub)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_alert_triangle),
+            tint = HandyTheme.colors.bgStatusNegative
+        )
+        Text(
+            text = text,
+            color = HandyTheme.colors.textStatusNegative,
+            maxLines = 2,
+            style = HandyTypography.B3Sb14
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(
+            painter = painterResource(id = R.drawable.ic_cancel),
+            tint = HandyTheme.colors.textBasicTertiary,
+        )
+    }
+}
