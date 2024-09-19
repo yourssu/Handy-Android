b5eb37c14f3beaf6aa3c14abcc506c2e7252ce76
LEE YOU BIN <youbeene3409@naver.com>
2024-09-20 03:25:09 +0900

    feat : make snackbar preview file

---
+++ app/src/main/kotlin/com/yourssu/handy/demo/SnackBarPreview.kt
@@ -0,0 +1,13 @@
+package com.yourssu.handy.demo
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.yourssu.handy.compose.HandyTheme
+
+@Preview(showBackground = true)
+@Composable
+fun SnackBarPreview() {
+    HandyTheme {
+
+    }
+}
--- compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
@@ -1,11 +1,12 @@
 package com.yourssu.handy.compose
 
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SnackBar(
-    modifier : Modifier = Modifier
-){
+    modifier: Modifier = Modifier
+) {
 
 }
