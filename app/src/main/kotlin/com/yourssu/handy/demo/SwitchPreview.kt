844bc6d33132ad2b5757099e11db97074b886e58
kwak kun <gael.urssu@gmail.com>
2024-09-21 23:45:01 +0900

    chore : Preview 굳이 프리뷰파라미터로 만들 필요 없을거 같아서 Column으로 나타냄

--- app/src/main/kotlin/com/yourssu/handy/demo/SwitchPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/SwitchPreview.kt
@@ -1,15 +1,11 @@
 package com.yourssu.handy.demo
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import com.yourssu.handy.compose.Switch
 import com.yourssu.handy.compose.SwitchSize
 import com.yourssu.handy.compose.SwitchState
@@ -17,17 +13,16 @@ import com.yourssu.handy.compose.Text
 
 
 data class SwitchPreviewParameter(
-    val switchState: SwitchState,
+    var switchState: SwitchState,
     val switchSize: SwitchSize
 )
 
-class SwitchPreviewParameterProvider : PreviewParameterProvider<SwitchPreviewParameter> {
-    override val values = getAllCombinations()
+@Preview(showBackground = true)
+@Composable
+private fun PreviewMultipleSwitchState() {
+    val samples = remember { mutableStateListOf<SwitchPreviewParameter>() }
 
-    private fun getAllCombinations(): Sequence<SwitchPreviewParameter> {
-        val samples = mutableListOf<SwitchPreviewParameter>()
-
-        // SwitchState와 SwitchSize의 모든 조합 생성
+    LaunchedEffect(Unit) {// SwitchState와 SwitchSize의 모든 조합 생성
         val switchStates =
             listOf(SwitchState.Unselected, SwitchState.Selected, SwitchState.Disabled)
         val switchSizes = listOf(SwitchSize.Large, SwitchSize.Medium, SwitchSize.Small)
@@ -42,36 +37,19 @@ class SwitchPreviewParameterProvider : PreviewParameterProvider<SwitchPreviewPar
                 )
             }
         }
-        return samples.asSequence()
     }
-}
 
-@Preview
-@Composable
-private fun PreviewSwitchState() {
-    var switchState by remember { mutableStateOf<SwitchState>(SwitchState.Unselected) }
 
     Column {
-        Text(text = "Size:Large State:$switchState")
-        Switch(
-            switchState = switchState,
-            switchSize = SwitchSize.Large,
-            onToggle = { switchState = it }
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun PreviewMultipleSwitchState(
-    @PreviewParameter(SwitchPreviewParameterProvider::class) parameter: SwitchPreviewParameter,
-) {
-    Column {
-        Text(text = "Size:${parameter.switchSize} State:${parameter.switchState}")
-        Switch(
-            switchState = parameter.switchState,
-            switchSize = parameter.switchSize,
-            onToggle = { Log.d("Switch", "onToggle: $it") }
-        )
+        samples.forEachIndexed { index, switchPreviewParameter ->
+            Text(text = "Size:${switchPreviewParameter.switchSize} State:${switchPreviewParameter.switchState}")
+            Switch(
+                switchState = switchPreviewParameter.switchState,
+                switchSize = switchPreviewParameter.switchSize,
+                onToggle = {
+                    samples[index] = samples[index].copy(switchState = it)
+                }
+            )
+        }
     }
 }
