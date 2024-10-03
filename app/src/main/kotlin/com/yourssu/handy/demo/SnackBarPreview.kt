7ab14c56acb2da181eb3ace8ebea15ca3f44d0f8
LEE YOU BIN <youbeene3409@naver.com>
2024-10-04 03:35:48 +0900

    refactor : refactor snackBar code

--- app/src/main/kotlin/com/yourssu/handy/demo/SnackBarPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/SnackBarPreview.kt
@@ -8,9 +8,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.yourssu.handy.compose.ErrorSnackBarItem
+import com.yourssu.handy.compose.ErrorSnackBar
 import com.yourssu.handy.compose.HandyTheme
-import com.yourssu.handy.compose.InfoSnackBarItem
+import com.yourssu.handy.compose.InfoSnackBar
 
 @Preview(showBackground = true)
 @Composable
@@ -19,11 +19,11 @@ fun InfoSnackBarPreview() {
         Column(
             modifier = Modifier.padding(10.dp)
         ) {
-            InfoSnackBarItem(
+            InfoSnackBar(
                 text = "한 줄짜리 정보성 메세지가 들어갑니다."
             )
             Spacer(modifier = Modifier.height(10.dp))
-            InfoSnackBarItem(
+            InfoSnackBar(
                 text = "줄 수가 두 줄 이상이 되는 스낵바 메시지입니다.\n좌측 정렬을 해주세요."
             )
         }
@@ -37,12 +37,12 @@ fun ErrorSnackBarPreview() {
         Column(
             modifier = Modifier.padding(10.dp)
         ) {
-            ErrorSnackBarItem(
+            ErrorSnackBar(
                 text = "에러 메세지가 들어갑니다",
                 onClick = {}
             )
             Spacer(modifier = Modifier.height(10.dp))
-            ErrorSnackBarItem(
+            ErrorSnackBar(
                 text = "두 줄 이상의 에러 메세지가 들어갈 경우\n 아이콘은 모두 위로 정렬해주세요.",
                 onClick = {}
             )
--- compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/SnackBar.kt
@@ -1,12 +1,5 @@
 package com.yourssu.handy.compose
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -17,30 +10,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import com.yourssu.handy.compose.foundation.HandyTypography
 import com.yourssu.handy.compose.icons.HandyIcons
 import com.yourssu.handy.compose.icons.filled.AlertTriangle
 import com.yourssu.handy.compose.icons.line.Close
-import kotlinx.coroutines.delay
 
 /**
  * 정보성 스낵바의 UI를 그린 함수입니다.
+ *
+ * 유저의 행동에 대한 단순 결과를 나타낼 때 사용합니다.
+ * 특정 시간 노출 후에 사라집니다.
  *
  * @param text 스낵바의 문구를 나타내는 텍스트, 최대 두 줄까지 입력 가능
  * @param modifier Modifier
  */
 @Composable
-fun InfoSnackBarItem(
+fun InfoSnackBar(
     text: String,
     modifier: Modifier = Modifier,
 ) {
@@ -62,63 +51,19 @@ fun InfoSnackBarItem(
 }
 
 /**
- * 정보성 스낵바를 구현한 함수입니다.
- *
- * 유저의 행동에 대한 단순 결과를 나타낼 때 사용합니다.
- * 특정 시간 노출 후에 사라집니다.
- *
- * @param text 스낵바의 문구를 나타내는 텍스트, 최대 두 줄까지 입력 가능
- * @param onDismiss 스낵바가 사라질 때 호출되는 함수
- * @param duration 스낵바가 지속되어 있는 시간 (기본값은 5초)
- */
-@Composable
-fun InfoSnackBar(
-    text: String,
-    onDismiss: () -> Unit,
-    duration: Long = SNACK_BAR_DURATION,
-) {
-    var visible by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        visible = true
-        delay(duration)
-        visible = false
-        delay(FADE_OUT_DELAY)
-        onDismiss()
-    }
-
-    Popup(
-        alignment = Alignment.BottomCenter,
-    ) {
-        AnimatedVisibility(
-            visible = visible,
-            enter = fadeIn(
-                animationSpec = tween(durationMillis = FADE_IN_DURATION)
-            ) + expandVertically(
-                expandFrom = Alignment.Top
-            ),
-            exit = fadeOut(
-                animationSpec = tween(durationMillis = FADE_OUT_DURATION)
-            ) + slideOutVertically(
-                targetOffsetY = { fullHeight -> fullHeight },
-            )
-        ) {
-            InfoSnackBarItem(
-                text = text,
-            )
-        }
-    }
-}
-
-/**
  * 에러 스낵바의 UI를 그린 함수입니다.
+ *
+ * 사용자의 수행 과정에 부정적인 결과가 발생하거나
+ * 정보성 스낵바보다 강조해야 할 메시지를 담아야 할 때 사용합니다.
+ *
+ * X 버튼을 눌러야만 사라집니다.
  *
  * @param text 스낵바의 문구를 나타내는 텍스트, 최대 두 줄까지 입력 가능
  * @param onClick 스낵바의 X 버튼을 눌렀을 때 호출되는 함수
  * @param modifier Modifier
  */
 @Composable
-fun ErrorSnackBarItem(
+fun ErrorSnackBar(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
@@ -146,7 +91,7 @@ fun ErrorSnackBarItem(
         )
         Spacer(modifier = Modifier.weight(1f))
         Icon(
-            imageVector =  HandyIcons.Line.Close,
+            imageVector = HandyIcons.Line.Close,
             tint = HandyTheme.colors.textBasicTertiary,
             modifier = Modifier
                 .clickable(onClick = onClick)
@@ -155,61 +100,9 @@ fun ErrorSnackBarItem(
     }
 }
 
-/**
- * 에러 스낵바를 구현한 함수입니다.
- *
- * 사용자의 수행 과정에 부정적인 결과가 발생하거나
- * 정보성 스낵바보다 강조해야 할 메시지를 담아야 할 때 사용합니다.
- *
- * X 버튼을 눌러야만 사라집니다.
- *
- * @param text 스낵바의 문구를 나타내는 텍스트, 최대 두 줄까지 입력 가능
- * @param onDismiss 스낵바가 사라질 때 호출되는 함수
- */
-@Composable
-fun ErrorSnackBar(
-    text: String,
-    onDismiss: () -> Unit
-) {
-    var visible by remember { mutableStateOf(false) }
-
-    LaunchedEffect(Unit) {
-        visible = true
-    }
-
-    LaunchedEffect(visible) {
-        if (!visible) {
-            delay(FADE_OUT_DELAY)
-            onDismiss()
-        }
-    }
-
-    Popup(
-        alignment = Alignment.BottomCenter
-    ) {
-        AnimatedVisibility(
-            visible = visible,
-            enter = fadeIn(
-                animationSpec = tween(durationMillis = FADE_IN_DURATION)
-            ) + expandVertically(
-                expandFrom = Alignment.Top
-            ),
-            exit = shrinkVertically(
-                shrinkTowards = Alignment.Bottom,
-                animationSpec = tween(durationMillis = FADE_OUT_DURATION)
-            ) + slideOutVertically(
-                targetOffsetY = { fullHeight -> fullHeight }
-            )
-        ) {
-            ErrorSnackBarItem(
-                text = text,
-                onClick = { visible = false }
-            )
-        }
-    }
+object SnackBarDefaults {
+    const val SNACK_BAR_DURATION = 5000L
+    const val FADE_IN_DURATION = 500
+    const val FADE_OUT_DURATION = 300
+    const val TARGET_VALUE = -16f
 }
-
-private const val SNACK_BAR_DURATION = 5000L
-private const val FADE_OUT_DELAY = 300L
-private const val FADE_IN_DURATION = 500
-private const val FADE_OUT_DURATION = 300
