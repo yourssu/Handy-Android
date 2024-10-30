f81ac4257340ca8cde95f23124361efac79f19b4
kwak kun <gael.urssu@gmail.com>
2024-10-30 23:52:07 +0900

    chore : SwitchPreview 업데이트. 간단하게 flatmap을 사용하는 방식으로

--- app/src/main/kotlin/com/yourssu/handy/demo/SwitchPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/SwitchPreview.kt
@@ -2,9 +2,6 @@ package com.yourssu.handy.demo
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
 import com.yourssu.handy.compose.Switch
 import com.yourssu.handy.compose.SwitchSize
@@ -20,23 +17,11 @@ data class SwitchPreviewParameter(
 @Preview(showBackground = true)
 @Composable
 private fun PreviewMultipleSwitchState() {
-    val samples = remember { mutableStateListOf<SwitchPreviewParameter>() }
-
-    LaunchedEffect(Unit) {// SwitchState와 SwitchSize의 모든 조합 생성
-        val switchStates =
-            listOf(SwitchState.Unselected, SwitchState.Selected, SwitchState.Disabled)
-        val switchSizes = listOf(SwitchSize.Large, SwitchSize.Medium, SwitchSize.Small)
-
-        switchStates.forEach { state ->
-            switchSizes.forEach { size ->
-                samples.add(
-                    SwitchPreviewParameter(
-                        switchState = state,
-                        switchSize = size
-                    )
-                )
-            }
-        }
+    val switchStates =
+        listOf(SwitchState.Unselected, SwitchState.Selected, SwitchState.Disabled)
+    val switchSizes = listOf(SwitchSize.Large, SwitchSize.Medium, SwitchSize.Small)
+    val samples = switchStates.flatMap { state ->
+        switchSizes.map { size -> SwitchPreviewParameter(state, size) }
     }
 
 
@@ -47,7 +32,7 @@ private fun PreviewMultipleSwitchState() {
                 switchState = switchPreviewParameter.switchState,
                 switchSize = switchPreviewParameter.switchSize,
                 onToggle = {
-                    samples[index] = samples[index].copy(switchState = it)
+
                 }
             )
         }
