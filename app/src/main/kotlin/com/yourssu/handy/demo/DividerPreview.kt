fc75581048bfbb213b9c411cb405d38bd505a5c4
Gael-Android <gael.urssu@gmail.com>
2024-11-22 14:30:31 +0900

    chore : 프리뷰수정

--- app/src/main/kotlin/com/yourssu/handy/demo/DividerPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/DividerPreview.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yourssu.handy.compose.Divider
 import com.yourssu.handy.compose.DividerSize
+import com.yourssu.handy.compose.HandyTheme
 import com.yourssu.handy.compose.Text
 
 @Preview(showBackground = true)
@@ -13,10 +14,18 @@ import com.yourssu.handy.compose.Text
 fun DividerExample() {
     Column {
         Text("Section 1")
-        Divider(dividerSize = DividerSize.TWO, width = 100.dp)
+        Divider(
+            dividerSize = DividerSize.TWO,
+            width = 100.dp,
+            color = HandyTheme.colors.bgBasicBlack
+        )
         Text("Section 2")
-        Divider(dividerSize = DividerSize.FOUR, width = 200.dp)
+        Divider(
+            dividerSize = DividerSize.FOUR,
+            width = 200.dp,
+            color = HandyTheme.colors.bgBasicBlack
+        )
         Text("Section 3")
-        Divider(dividerSize = DividerSize.EIGHT)
+        Divider(dividerSize = DividerSize.EIGHT, color = HandyTheme.colors.bgBasicBlack)
     }
 }
