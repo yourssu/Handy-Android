181f8c9bfc7e411e46bb38310c03f8efac114a63
Yu Jin <qldls0307@naver.com>
2024-12-09 14:00:19 +0900

    feat: LeftAlignedTopAppBar

--- compose/src/main/kotlin/com/yourssu/handy/compose/TopAppBar.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/TopAppBar.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,10 +23,12 @@ import androidx.compose.ui.unit.dp
 import com.yourssu.handy.compose.TopBarDefaults.topBarHeight
 import com.yourssu.handy.compose.TopBarDefaults.topBarHorizontalPadding
 import com.yourssu.handy.compose.button.TextButton
+import com.yourssu.handy.compose.foundation.HandyTypography
 import com.yourssu.handy.compose.icons.HandyIcons
 import com.yourssu.handy.compose.icons.filled.Add
 import com.yourssu.handy.compose.icons.filled.ArrowsChevronLeft
 import com.yourssu.handy.compose.icons.filled.List
+import com.yourssu.handy.compose.icons.line.Add
 
 sealed interface NavIcon {
     data object None : NavIcon
@@ -33,20 +37,92 @@ sealed interface NavIcon {
 }
 
 /**
- * Center-Aligned Top BAr
+ * Center-Aligned Top App Bar
  *
- * 타이틀이 좌측에 위치한 탑앱바입니다.
- * 기능의 첫 페이지에 사용되는 탑앱바로, 현재 페이지의 제목을 나타낼 때 사용합니다.
+ * 타이틀이 중앙에 위치한 탑앱바입니다. 좌측에 메뉴 아이콘 및 뒤로 가기 아이콘이 요구될 경우 사용합니다.
  * 아이콘으로 기능을 명확하게 표현하지 못하거나 확실하게 기능을 설명하고 싶을 때 아이콘 대신 Text를 사용할 수 있습니다.
+ * 이때, 텍스트의 사용은 한 개까지만 가능하며, 텍스트가 공백 포함 5자일 경우 우측에 아이콘을 같이 쓸 수 없습니다.
  *
- * @param title headline or Logo 최대 9자(공백 포함)
- * @param navIcon 왼쪽 아이콘 (None, Menu, Back)
- * @param actions 오른쪽 Trailing Button (icon or text)
+ *
+ * @param headline headline or Logo 최대 9자(공백 포함)
+ * @param navIcon 왼쪽 아이콘 NavIcon.None, NavIcon.Menu, NavIcon.Back
+ * @param trailingButton 오른쪽 아이콘 or 텍스트 (아이콘은 임의로 변경할 수 있으며 Center-aligned의 우측엔 최대 2개의 아이콘 버튼)
  **/
 @Composable
 fun CenterAlignedTopAppBar(
-    title: String,
+    headline: String,
     navIcon: NavIcon,
+    trailingButton: @Composable (RowScope.() -> Unit)? = null
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = HandyTheme.colors.bgBasicDefault)
+            .height(topBarHeight),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = headline,
+            color = HandyTheme.colors.textBasicPrimary,
+            style = HandyTypography.T2Sb18,
+            textAlign = TextAlign.Center,
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = topBarHorizontalPadding), //Top App Bar의 높이값을 임의로 변경하지 않습니다.
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Left Icon
+            IconButton(navIcon)
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Actions
+            if (trailingButton != null) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxHeight()
+                ) {
+                    trailingButton()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RowScope.IconButton(navIcon: NavIcon) {
+    when (navIcon) {
+        is NavIcon.Back -> Icon(
+            imageVector = HandyIcons.Filled.ArrowsChevronLeft,
+            contentDescription = "Back",
+            modifier = Modifier.clickable(onClick = navIcon.onClick)
+        )
+
+        is NavIcon.Menu -> Icon(
+            imageVector = HandyIcons.Filled.List,
+            contentDescription = "Menu",
+            modifier = Modifier.clickable(onClick = navIcon.onClick)
+        )
+
+        NavIcon.None -> Spacer(modifier = Modifier.width(24.dp)) // 여백 확보
+    }
+}
+
+/**
+ * Left-aligned
+ *
+ * 타이틀이 좌측에 위치한 탑앱바입니다. 기능의 첫 페이지에 사용되는 탑앱바로,
+ * 현재 페이지의 제목을 나타낼 때 사용합니다. 아이콘으로 기능을 명확하게 표현하지 못하거나 확실하게 기능을 설명하고 싶을 때 아이콘 대신 Text를 사용할 수 있습니다.
+ *
+ * @param title headline or Logo 최대 9자(공백 포함)
+ * @param actions 오른쪽 아이콘 or 텍스트 (아이콘은 임의로 변경할 수 있으며 Left-aligned의 우측엔 최대 3개의 아이콘 버튼)
+ **/
+@Composable
+fun LeftAlignedTopAppBar(
+    title: String,
     actions: @Composable (RowScope.() -> Unit)? = null
 ) {
     Box(
@@ -56,7 +132,6 @@ fun CenterAlignedTopAppBar(
             .height(topBarHeight),
         contentAlignment = Alignment.Center
     ) {
-        // 타이틀
         Row(
             modifier = Modifier
                 .fillMaxSize()
@@ -65,68 +140,116 @@ fun CenterAlignedTopAppBar(
         ) {
             Text(
                 text = title,
+                style = HandyTypography.H3Sb24,
                 color = HandyTheme.colors.textBasicPrimary,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.weight(2f) // 텍스트가 중앙에 위치하도록 weight 추가
             )
-        }
 
+            Spacer(modifier = Modifier.weight(1f))
 
-        Row(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = topBarHorizontalPadding),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-
-            // 왼쪽 아이콘 navIcon
-            when (navIcon) {
-                is NavIcon.Back -> Icon(HandyIcons.Filled.ArrowsChevronLeft)
-                is NavIcon.Menu -> Icon(HandyIcons.Filled.List)
-                NavIcon.None -> {}
+            // Actions
+            if (actions != null) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(8.dp)
+                ) {
+                    actions()
+                }
             }
-
-            Spacer(modifier = Modifier.weight(1f)) // 가운데 공간 확보
-
-            // 오른쪽 액션 버튼들
-            actions?.invoke(this)
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CenterAlignedTopAppBarPreview() {
+    Column {
+        CenterAlignedTopAppBar(
+            headline = "Centered",
+            navIcon = NavIcon.None,
+            trailingButton = {}
+        )
+
+        CenterAlignedTopAppBar(
+            headline = "Back Example",
+            navIcon = NavIcon.Back(onClick = { Log.d("TopBarPreview", "Back clicked") }),
+            trailingButton = {
+                TextButton(text = "Share", onClick = { Log.d("TopBarPreview", "Share clicked") })
+            }
+        )
+
+        CenterAlignedTopAppBar(
+            headline = "Menu Example",
+            navIcon = NavIcon.Menu(onClick = { Log.d("TopBarPreview", "Menu clicked") }),
+            trailingButton = {
+                Icon(
+                    imageVector = HandyIcons.Filled.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
+            }
+        )
     }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun TopBarPreview() {
-
+fun LeftAlignedTopBarPreview() {
     Column {
-        CenterAlignedTopAppBar(
-            title = "title",
-            navIcon = NavIcon.None,
-            actions = {
-
-            }
+        LeftAlignedTopAppBar(
+            title = "Title",
+            actions = {}
         )
-
-        CenterAlignedTopAppBar(
-            title = "제목을 입력해주세요",
-            navIcon = NavIcon.Back(onClick = {}),
-            actions = {
-                Icon(HandyIcons.Filled.Add, iconSize = IconSize.M)
-                TextButton(text = "공유하기", onClick = {})
-            }
-        )
-
-        CenterAlignedTopAppBar(
-            title = "유어슈 최고",
-            navIcon = NavIcon.Menu(onClick = {}),
+        LeftAlignedTopAppBar(
+            title = "Title",
             actions = {
                 Icon(
-                    HandyIcons.Filled.Add,
-                    modifier = Modifier.clickable { Log.d("TAG", "TopBarPreview: 클릭") })
-                Icon(HandyIcons.Filled.Add)
+                    imageVector = HandyIcons.Line.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
             }
         )
+        LeftAlignedTopAppBar(
+            title = "Title",
+            actions = {
+                Icon(
+                    imageVector = HandyIcons.Line.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
+                Icon(
+                    imageVector = HandyIcons.Line.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
+            }
+        )
+        LeftAlignedTopAppBar(
+            title = "Title",
+            actions = {
+                Icon(
+                    imageVector = HandyIcons.Line.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
+                Icon(
+                    imageVector = HandyIcons.Line.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
+                Icon(
+                    imageVector = HandyIcons.Line.Add,
+                    contentDescription = "Add",
+                    modifier = Modifier.clickable { Log.d("TopBarPreview", "Add clicked") }
+                )
+            }
+        )
+
+
     }
 }
 
