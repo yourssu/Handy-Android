8d19973cbb3ba8ca40e67a42c01fbe8a6533d40d
Yu Jin <qldls0307@naver.com>
2024-12-11 17:11:01 +0900

    feat: OneButtonDialog content, boxbutton fillMaxWidth 구현

--- compose/src/main/kotlin/com/yourssu/handy/compose/Dialog.kt
+++ compose/src/main/kotlin/com/yourssu/handy/compose/Dialog.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,15 +27,14 @@ import com.yourssu.handy.compose.icons.filled.Close
 
 
 /**
- * BaseButton : ripple 효과가 없는 Composable 함수 입니다.
- *
- * BoxButton, TextButton의 베이스가 됩니다.
+ * OneButtonDialog : 선택 버튼이 하나 있는 Dialog 입니다.
  *
  * @param title Dialog 제목 text
  * @param description Dialog 안의 설명 text (optional)
- * @param subDescription Dialog 안의 추가 설명 text (optional)
+ * @param content Dialog 안의 content. 주로 이미지가 들어감(optional)
  * @param positiveText 확인 버튼 text
  * @param onPositiveClick 확인 버튼 클릭 시 실행되는 함수
+ * @param onDismiss 닫기(X) 버튼 클릭시 실행되는 함수
  **/
 @Composable
 fun OneButtonDialog(
@@ -42,16 +44,17 @@ fun OneButtonDialog(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
     description: String = "",
-    subDescription: String = "",
+    content: @Composable (() -> Unit)? = null,
 ) {
     Box(
         modifier = modifier
-            .background(color = HandyTheme.colors.bgBasicDefault)
-//            .width(196.dp)
-            .padding(20.dp)
+            .padding(16.dp)
+            .background(color = HandyTheme.colors.bgBasicDefault, shape = RoundedCornerShape(16.dp))
+            .fillMaxWidth()
+            .padding(20.dp),
     ) {
         Column {
-            Row {
+            Row(modifier.align(Alignment.CenterHorizontally)) {
                 Text(
                     text = title,
                     style = HandyTypography.T1Sb20,
@@ -69,26 +72,23 @@ fun OneButtonDialog(
 
             Spacer(modifier = modifier.height(20.dp))
 
-            if (subDescription.isNotEmpty()) {
-                Text(text = subDescription)
+            if (content != null) {
+                content()
                 Spacer(modifier = modifier.height(20.dp))
             }
 
             BoxButton(
+                modifier = modifier.fillMaxWidth(),
                 text = positiveText, onClick = onPositiveClick,
-                sizeType = BoxButtonSize.XL,
+                sizeType = BoxButtonSize.L,
             )
         }
-
     }
-
 }
 
 
 /**
- * BaseButton : ripple 효과가 없는 Composable 함수 입니다.
- *
- * BoxButton, TextButton의 베이스가 됩니다.
+ * TwoButtonDialog : 버튼이 두개 았는 다이알로그 입니다.
  *
  * @param title Dialog 제목 text
  * @param description Dialog 안의 설명 text
@@ -96,6 +96,7 @@ fun OneButtonDialog(
  * @param negativeText 취소 버튼 text
  * @param onPositiveClick 확인 버튼 클릭 시 실행되는 함수
  * @param onNegativeClick 취소 버튼 클릭 시 실행되는 함수
+ * @param onDismiss 닫기(X) 버튼 클릭시 실행되는 함수
  **/
 @Composable
 fun TwoButtonDialog(
@@ -105,6 +106,7 @@ fun TwoButtonDialog(
     negativeText: String,
     onPositiveClick: () -> Unit,
     onNegativeClick: () -> Unit,
+    onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
 
@@ -131,7 +133,6 @@ private fun DialogPreview() {
             OneButtonDialog(
                 title = "제목이 들어갑니다",
                 description = "내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다",
-                subDescription = "내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다 내용이 들어갑니다",
                 positiveText = "버튼",
                 onPositiveClick = {},
                 onDismiss = { showDialog = false } // 다이얼로그 닫기
