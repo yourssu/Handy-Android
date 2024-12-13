b2d474750e7e3aac5c0f5c62957c0a13b315c5cb
Yu Jin <qldls0307@naver.com>
2024-12-13 16:22:30 +0900

    feat: screen에서 띄우는 preview 추가

--- app/src/main/kotlin/com/yourssu/handy/demo/DialogPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/DialogPreview.kt
@@ -1,13 +1,17 @@
 package com.yourssu.handy.demo
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -15,6 +19,7 @@ import com.yourssu.handy.compose.HandyTheme
 import com.yourssu.handy.compose.Icon
 import com.yourssu.handy.compose.OneButtonDialog
 import com.yourssu.handy.compose.TwoButtonDialog
+import com.yourssu.handy.compose.button.BoxButton
 import com.yourssu.handy.compose.icons.HandyIcons
 import com.yourssu.handy.compose.icons.filled.Add
 
@@ -99,6 +104,48 @@ private fun TwoButtonDialogPreview() {
                 negativeText = "버튼1",
                 onNegativeClick = {},
                 content = { Icon(HandyIcons.Filled.Add) }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun OneButtonDialogOnScreenPreview() {
+
+    var showOneButtonDialog by remember { mutableStateOf(false) }
+    var showTwoButtonDialog by remember { mutableStateOf(false) }
+
+    HandyTheme {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            BoxButton(text = "원버튼 다이알로그", onClick = { showOneButtonDialog = true })
+            Spacer(modifier = Modifier.height(10.dp))
+            BoxButton(text = "투버튼 다이알로그", onClick = { showTwoButtonDialog = true })
+        }
+
+        if (showOneButtonDialog) {
+            OneButtonDialog(
+                title = "제목이 들어갑니다",
+                description = "내용",
+                positiveText = "버튼",
+                onPositiveClick = { showOneButtonDialog = false },
+                onDismiss = { showOneButtonDialog = false }
+            )
+        } else if (showTwoButtonDialog) {
+            TwoButtonDialog(
+                title = "제목이 들어갑니다",
+                description = "내용",
+                positiveText = "버튼",
+                onPositiveClick = { showTwoButtonDialog = false },
+                onDismiss = { showTwoButtonDialog = false },
+                negativeText = "버튼",
+                onNegativeClick = { showTwoButtonDialog = false }
             )
         }
     }
