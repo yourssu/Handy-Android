c2bf0e94b280ebc6ebcb0afed89acc9ea739e609
kangyuri1114 <yuri3334@gmail.com>
2025-01-19 20:05:41 +0900

    feat: mutableStateOf import 하기

--- app/src/main/kotlin/com/yourssu/handy/demo/OutlinedTextFieldPreview.kt
+++ app/src/main/kotlin/com/yourssu/handy/demo/OutlinedTextFieldPreview.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,7 +25,7 @@ fun OutlinedTextFieldPreview() {
                 .padding(20.dp),
             verticalArrangement = Arrangement.spacedBy(20.dp)
         ) {
-            val (text, onValueChange) = remember { androidx.compose.runtime.mutableStateOf("") }
+            val (text, onValueChange) = remember { mutableStateOf("") }
             OutlinedTextField(
                 value = text,
                 placeholder = "placeholder",
